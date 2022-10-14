d18e281a83a70826e6c79c1ac17b05c2586a14a9
Nikolai1312 <nicolasedesouza@gmail.com>
2022-10-14 01:32:54 -0300

    Código dia 1.

---
+++ desafio-alura.js
@@ -0,0 +1,18 @@
+let numeroUm = 1;
+let digitoUm = '1';
+let numeroVinte = 20;
+let digitoVinte = '20';
+let numeroDez = 10;
+let digitoDez = '10';
+
+if (numeroUm == digitoUm) {
+    console.log("Os dados são iguais, porém formatos diferentes.");
+} else {
+    console.log("Não são iguais");
+}
+
+if (numeroVinte == digitoVinte) {
+    console.log("Os dados são iguais, porém formatos diferentes.");
+} else {
+    console.log("Não são iguais");
+}
