0676be7c30cea858f96cf491af21a0db4a9aa3ff
Nikolai1312 <nicolasedesouza@gmail.com>
2022-11-19 15:32:17 -0300

    Conhecendo concatenação de itens em listas de Python.

---
+++ listas_dois.py
@@ -0,0 +1,5 @@
+declaracao = ['Eu', 'Helena', 'amo', 'a', 'odeio', 'gosto', " "]
+
+carta_de_amor = print(declaracao[0] + declaracao[6] + declaracao[2] + declaracao[6] + declaracao[3] + declaracao[6] + declaracao[1])
+
+# print(carta_de_amor)
